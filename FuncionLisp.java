dd0869a8bdde32470475e9a0df179ce66892e228
LuisPHernandez <lphm0105@gmail.com>
2025-03-20 12:16:25 -0600

    Creacion de la funcion

---
+++ FuncionLisp.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class FuncionLisp {
+    /**
+     * Lista de parámetros de la función
+     */
+    private List<String> parametros;
+    
+    /**
+     * Cuerpo de la función
+     */
+    private List<Object> cuerpo;
+
+    public FuncionLisp(List<String> parametros, List<Object> cuerpo) {
+        this.parametros = parametros;
+        this.cuerpo = cuerpo;
+    }
+
+    /**
+     * Obtiene la lista de parámetros de la función
+     * 
+     * @return Lista de parámetros
+     */
+    public List<String> getParametros() {
+        return parametros;
+    }
+
+    /**
+     * Obtiene el cuerpo de la función
+     * 
+     * @return Cuerpo de la función como una lista de expresiones
+     */
+    public List<Object> getCuerpo() {
+        return cuerpo;
+    }
+}
