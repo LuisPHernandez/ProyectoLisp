55d12460f2d12afa60efb7b09324b42bb9f4dfb0
LuisPHernandez <lphm0105@gmail.com>
2025-03-22 18:36:45 -0600

    Test para las funciones

---
+++ FuncionLispTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class FuncionLispTest {
+    @Test
+    public void testConstructorAndGetters() {
+        // Prepara los datos
+        List<String> parametros = Arrays.asList("a", "b", "c");
+        List<Object> cuerpo = Arrays.asList(
+                Arrays.asList("+", "a", "b"),  // First expression: (+ a b)
+                Arrays.asList("*", "c", 2)     // Second expression: (* c 2)
+        );
+
+        // Crea una instancia
+        FuncionLisp funcion = new FuncionLisp(parametros, cuerpo);
+
+        // Test getParametros
+        assertEquals("los parametros deben ser iguales", parametros, funcion.getParametros());
+
+        // Test getCuerpo
+        assertEquals("el cuerpo debe de ser igual", cuerpo, funcion.getCuerpo());
+    }
+}
